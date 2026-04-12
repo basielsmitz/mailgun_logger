acbd77cced1e6f88ec64a9a5ac870488ef2b57e0
basiel smitz <basiel.smitz@gmail.com>
2026-04-12 17:31:56 +0200

    no SSL on localhost (WSL issue)

--- config/dev.exs
+++ config/dev.exs
@@ -1,18 +1,16 @@
 import Config
 
-port = System.get_env("PORT") || "7070"
+port = String.to_integer(System.get_env("PORT") || "5050")
 
 config :mailgun_logger, MailgunLoggerWeb.Endpoint,
   url: [
-    host: System.get_env("HOST", "0.0.0.0"),
-    scheme: "https",
+    host: System.get_env("HOST", "localhost"),
+    scheme: "http",
     port: port
   ],
-  https: [
-    port: port,
-    cipher_suite: :strong,
-    keyfile: "priv/cert/selfsigned_key.pem",
-    certfile: "priv/cert/selfsigned.pem"
+  http: [
+    ip: {0, 0, 0, 0},
+    port: port
   ],
   live_reload: [
     web_console_logger: true,
@@ -28,15 +26,13 @@ config :mailgun_logger, MailgunLoggerWeb.Endpoint,
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ],
-  secret_key_base: "rnd"
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || "rnd"
 
 config :ex_aws,
   raw_path: "_dev_mailgun_logger/messages"
 
 config :mailgun_logger, MailgunLogger.Scheduler, jobs: []
-# config :mailgun_logger, MailgunLogger.Mailer, adapter: Bamboo.LocalAdapter
 
-# Do not include metadata nor timestamps in development logs
 config :logger, :default_formatter, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
